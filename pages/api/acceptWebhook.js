b9aefe3aaa200afe6754a8a174591af0c3e32da5
thisisjofrank <jofranchetti@gmail.com>
2021-03-01 20:15:03 +0000

    creating endpoint

---
+++ pages/api/acceptWebhook.js
@@ -0,0 +1,39 @@
+import Ably from "ably/promises";
+
+export default async function handler(req, res) {
+    
+    const incomingData = getSmsDetails(req, res);
+
+    if (!incomingData.success) {  
+        res.status(400).end();
+        return;
+    }
+
+    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+    const channel = client.channels.get("sms-notifications");
+    await channel.publish(incomingData);
+        
+    res.send(incomingData);    
+    res.status(200).end();
+};
+
+function getSmsDetails(req, res) {
+
+    const params = req.body;
+
+    if (!params.to || !params.msisdn) {
+        console.log('This is not a valid inbound SMS message!');
+        return { success: false };
+    }           
+    
+    return {
+        success: true,
+        messageId: params.messageId,
+        from: params.msisdn,
+        text: params.text,
+        type: params.type,
+        timestamp: params['message-timestamp']
+    };
+ 
+}
+ 
